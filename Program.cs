5b7c527a37975381104dcd7be4d13d87803fef6a
zpsle <zpsle@DESKTOP-ELOIINN>
2023-01-31 23:53:46 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿
+Console.WriteLine("Enter an integer between 1 and 100: ");
+try
+{
+    int input = int.Parse(Console.ReadLine());
+    Console.WriteLine("Specify the series type: (Even or Odd) ");
+    string series = Console.ReadLine();
+
+    if (series == "Odd" || series == "odd")
+    {
+        Console.WriteLine("You have selected " + series + " series. The numbers odd between 0 and " + input + " are:");
+        for (int i = 0; i <= input; i = i + 2)
+        {
+            Console.WriteLine(i);
+        }
+    }
+
+    if (series == "Even" || series == "even")
+    {
+        Console.WriteLine("You have selected " + series + " series. The even numbers between 0 and " + input + " are:");
+        for (int i = 0; i <= input; i = i + 2)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
+
+catch
+{
+    Console.WriteLine("Please enter an integer between 1 and 100: ");
+}
